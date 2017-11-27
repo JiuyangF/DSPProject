436e6c9828d774e60928dd90a5302adedec7d7a4
jiuyangF <1635375337@qq.com>
2017-11-27 12:00:20 +0800

    测试pychrom

---
+++ DSPProject/__init__.py
@@ -0,0 +1,5 @@
+import pymysql
+pymysql.install_as_MySQLdb()
+
+# from mongoengine import connect
+# connect('test', host='127.0.0.1')
---
+++ main.py
@@ -0,0 +1,5 @@
+import execute,cmd
+
+
+if __name__ == '__main__':
+    cmd.Cmd('')
